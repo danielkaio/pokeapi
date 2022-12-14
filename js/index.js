7585e8b907e5a9baf58a79ecaaf62407a5e34a8e
daniel <dani.ajala@yahoo.com>
2022-12-13 21:23:41 -0300

    mudança no html

--- js/index.js
+++ js/index.js
@@ -5,28 +5,17 @@ async function busca(nome){
         
         const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
         const c = await url.json()
-        console.log(c)
-
         
-       
         let p = document.querySelector('p')
         p.innerText = c.name
 
         let img = document.querySelector('img')
         img.setAttribute('src',c.sprites.other.dream_world.front_default)
 
-      
-
     } catch (erro) {
-        
         console.log(erro)
     }
-
 }
-
-
-
-
 
 
 let nome  = document.querySelector('.nome')
