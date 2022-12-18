4df4a256d41367d07b9fcaddad18791e6a7e077e
daniel <dani.ajala@yahoo.com>
2022-12-18 01:01:35 -0300

    atualização layout

--- js/index.js
+++ js/index.js
@@ -5,13 +5,22 @@ async function busca(nome){
         
         const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
         const c = await url.json()
+        console.log(c)
         
         
         let p = document.querySelector('p')
         p.innerText = c.name
 
+     
+    
         let img = document.querySelector('img')
         img.setAttribute('src',c.sprites.other.dream_world.front_default)
+        
+
+       
+
+        let span = document.querySelector('span')
+        span.innerText = c.abilities[0].ability.name
 
     } catch (erro) {
         console.log(erro)
